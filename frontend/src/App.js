a7dc65af903152731c43caed9c59755bf116ca53
Jordan Lee <airjlee04@gmail.com>
2025-06-29 01:49:47 -0700

    Added socratic dialogue pilot

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Home from './components/Home/Home';
 import Assignment from './components/Assignment/Assignment';
+import SocraticDialogue from './components/SocraticDialogue/SocraticDialogue';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'assignment':
+        return <Assignment onNavigateHome={() => setCurrentPage('home')} />;
+      case 'socratic':
+        return <SocraticDialogue onNavigateHome={() => setCurrentPage('home')} />;
+      default:
+        return <Home onNavigateToAssignment={(type) => setCurrentPage(type)} />;
+    }
+  };
+
   return (
     <div className="App">
-      <Assignment />
+      {renderPage()}
     </div>
   );
 }
